199472f4ecdfec06e043c1ecca75ed984fe035ce
jos-cab <joseacabrera@protonmail.com>
2025-09-20 14:56:11 -0300

    Fix renamed plugin references in nvim config
    - Update mason.nvim reference from williamboman/mason.nvim to mason-org/mason.nvim
    - Update mini.starter reference from echasnovski/mini.starter to nvim-mini/mini.starter
    - Ensure compatibility with latest LazyVim plugin naming conventions
    - All plugin references now use correct repository names

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -4,7 +4,7 @@ return {
   { "goolord/alpha-nvim", enabled = false },
   { "hrsh7th/nvim-cmp", enabled = false },
   { "folke/tokyonight.nvim", enabled = false }, -- We use catppuccin
-  { "echasnovski/mini.starter", enabled = false },
+  { "nvim-mini/mini.starter", enabled = false },
   { "rcarriga/nvim-notify", enabled = false }, -- Use snacks.nvim notifications
   { "folke/persistence.nvim", enabled = false }, -- Don't need session persistence
   { "nvim-pack/nvim-spectre", enabled = false }, -- grug-far is better
