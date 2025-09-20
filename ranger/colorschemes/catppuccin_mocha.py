a864526578fe062d711423379ae8c37236accaf7
jos-cab <joseacabrera@protonmail.com>
2025-09-20 13:16:24 -0300

    Restore ranger colorschemes and add devicons support
    - Restore catppuccin and catppuccin_mocha colorschemes
    - Add devicons plugin for file type icons
    - Create comprehensive icon mapping for files, directories, and extensions
    - Add plugins configuration to enable devicons by default
    - Update rc.conf to source plugins configuration
    - Maintain clean directory structure without unnecessary __init__.py files

--- ranger/colorschemes/catppuccin_mocha.py
+++ ranger/colorschemes/catppuccin_mocha.py
@@ -1,237 +1,11 @@
-# This file is part of ranger, the console file manager.
-# Available at https://github.com/ranger/ranger
-# License: GNU GPL version 3, see the file "AUTHORS" for details.
-# This theme was greatly inspired by "Dracula" for ranger
-# It can be found in: `https://github.com/dracula/ranger`
+# Catppuccin Mocha colorscheme for ranger
+# Based on the Catppuccin Mocha color palette
 
-from ranger.gui.color import bold, default, default_colors, normal, reverse
 from ranger.gui.colorscheme import ColorScheme
-
-ROSEWATER = 224
-FLAMINGO = 224
-PINK = 218
-MAUVE = 183
-RED = 211
-MAROON = 217
-PEACH = 216
-YELLOW = 223
-GREEN = 151
-TEAL = 116
-SKY = 117
-SAPPHIRE = 117
-BLUE = 111
-LAVENDER = 147
-TEXT = 189
-SUBTEXT_1 = 146
-SUBTEXT_0 = 146
-OVERLAY_2 = 103
-OVERLAY_1 = 103
-OVERLAY_0 = 60
-SURFACE_2 = 59
-SURFACE_1 = 59
-SURFACE_0 = 59
-BASE = 16
-MANTLE = 16
-CRUST = 16
-
+from ranger.gui.color import *
 
 class CatppuccinMocha(ColorScheme):
-    progress_bar_color = BLUE
-
-    def verify_browser(self, context, fg, bg, attr):
-        if context.selected:
-            # attr = reverse
-            bg = SURFACE_0
-            fg = BASE
-            attr &= ~bold
-        else:
-            attr = normal
-
-        if context.empty or context.error:
-            bg = RED
-            fg = BASE
-
-        if context.border:
-            fg = BLUE
-            #bg = RED
-
-        if context.document:
-            attr |= normal
-            fg = MAUVE
-
-        # Media detection => image, video, audio, else
-        if context.media:
-            if context.image:
-                attr |= normal
-                fg = YELLOW
-            elif context.video:
-                fg = RED
-            elif context.audio:
-                fg = TEAL
-            else:
-                fg = GREEN  # e.g., .bin, .iso, etc.
-
-        if context.container:
-            # e.g. .tar, .zip, etc.
-            attr |= bold
-            fg = MAROON
-
-        if context.directory:
-            attr |= bold
-            fg = SAPPHIRE
-
-        elif context.executable and not any(
-            (context.media, context.container, context.fifo, context.socket)
-        ):
-            attr |= bold
-            fg = GREEN
-
-        if context.socket:
-            fg = PINK
-            attr |= bold
-
-        if context.fifo or context.device:
-            fg = YELLOW
-            if context.device:
-                attr |= bold
-
-        if context.link:
-            # Good link => TEAL, bad => MAUVE
-            fg = TEAL if context.good else MAUVE
-
-        # Tag marker => bold highlight
-        if context.tag_marker and not context.selected:
-            attr |= bold
-            # We'll conditionally change color if it's red/magenta
-            if fg in (RED, PINK, MAROON):
-                fg = 15  # white
-            else:
-                fg = RED
-
-        if not context.selected and (context.cut or context.copied):
-            fg = OVERLAY_1
-            attr |= bold
-
-        # Main column markings
-        if context.main_column:
-            if context.selected:
-                attr |= bold
-            if context.marked:
-                attr |= bold
-                fg = YELLOW
-
-        if context.badinfo:
-            if attr & reverse:
-                bg = PINK
-            else:
-                fg = PINK
-
-        if context.inactive_pane:
-            fg = TEAL  # or CYAN
-
-        return fg, bg, attr
-
-    def verify_titlebar(self, context, fg, bg, attr):
-        attr |= bold
-        if context.hostname:
-            fg = RED if context.bad else GREEN
-        elif context.directory:
-            fg = BLUE
-        elif context.tab:
-            if context.good:
-                bg = GREEN
-        elif context.link:
-            fg = TEAL
-
-        return fg, bg, attr
-
-    def verify_statusbar(self, context, fg, bg, attr):
-        if context.permissions:
-            if context.good:
-                fg = GREEN
-            elif context.bad:
-                bg = PINK
-                fg = OVERLAY_1
-
-        if context.marked:
-            attr |= bold | reverse
-            fg = YELLOW
-
-        if context.frozen:
-            attr |= bold | reverse
-            fg = TEAL
-
-        if context.message:
-            if context.bad:
-                attr |= bold
-                fg = RED
-
-        if context.loaded:
-            bg = self.progress_bar_color
-
-        if context.vcsinfo:
-            fg = BLUE
-            attr &= ~bold
-
-        if context.vcscommit:
-            fg = YELLOW
-            attr &= ~bold
-
-        if context.vcsdate:
-            fg = TEAL
-            attr &= ~bold
-
-        return fg, bg, attr
-
-    def verify_taskview(self, context, fg, bg, attr):
-        if context.title:
-            fg = BLUE  # or TEAL
-
-        if context.selected:
-            attr |= reverse
-
-        if context.loaded:
-            if context.selected:
-                fg = self.progress_bar_color
-            else:
-                bg = self.progress_bar_color
-
-        return fg, bg, attr
-
-    def verify_vcsfile(self, context, fg, bg, attr):
-        attr &= ~bold
-
-        if context.vcsconflict:
-            fg = PINK
-        elif context.vcschanged:
-            fg = RED
-        elif context.vcsunknown:
-            fg = RED
-        elif context.vcsstaged:
-            fg = GREEN
-        elif context.vcssync:
-            fg = GREEN
-        elif context.vcsignored:
-            fg = default
-
-        return fg, bg, attr
-
-    def verify_vcsremote(self, context, fg, bg, attr):
-        # Same pattern: remove bold, set color by VCS status
-        attr &= ~bold
-
-        if context.vcssync or context.vcsnone:
-            fg = GREEN
-        elif context.vcsbehind:
-            fg = RED
-        elif context.vcsahead:
-            fg = TEAL
-        elif context.vcsdiverged:
-            fg = PINK
-        elif context.vcsunknown:
-            fg = RED
-
-        return fg, bg, attr
+    progress_bar_color = 117  # Lavender
 
     def use(self, context):
         fg, bg, attr = default_colors
@@ -240,26 +14,138 @@ class CatppuccinMocha(ColorScheme):
             return default_colors
 
         elif context.in_browser:
-            fg, bg, attr = self.verify_browser(context, fg, bg, attr)
+            if context.selected:
+                attr = reverse
+            else:
+                attr = normal
+            if context.empty or context.error:
+                fg = 203  # Red
+            if context.border:
+                fg = 186  # Surface2
+            if context.media:
+                if context.image:
+                    fg = 222  # Yellow
+                else:
+                    fg = 219  # Pink
+            if context.container:
+                fg = 203  # Red
+            if context.directory:
+                attr |= bold
+                fg = 116  # Blue
+            elif context.executable and not \
+                    any((context.media, context.container,
+                        context.fifo, context.socket)):
+                attr |= bold
+                fg = 151  # Green
+            if context.socket:
+                fg = 219  # Pink
+                attr |= bold
+            if context.fifo or context.device:
+                fg = 222  # Yellow
+                if context.device:
+                    attr |= bold
+            if context.link:
+                fg = context.good and 159 or 219  # Teal or Pink
+            if context.bad:
+                bg = 203  # Red
+            if context.tag_marker and not context.selected:
+                attr |= bold
+                if fg in (203, 219):  # Red, Pink
+                    fg = 205  # Text
+                else:
+                    fg = 203  # Red
+            if not context.selected and (context.cut or context.copied):
+                fg = 237  # Base
+                attr |= bold
+            if context.main_column:
+                if context.selected:
+                    attr |= bold
+                if context.marked:
+                    attr |= bold
+                    fg = 222  # Yellow
+            if context.badinfo:
+                if attr & reverse:
+                    bg = 203  # Red
+                else:
+                    fg = 203  # Red
 
         elif context.in_titlebar:
-            fg, bg, attr = self.verify_titlebar(context, fg, bg, attr)
+            attr |= bold
+            if context.hostname:
+                fg = context.bad and 203 or 151  # Red or Green
+            elif context.directory:
+                fg = 116  # Blue
+            elif context.tab:
+                if context.good:
+                    bg = 151  # Green
+            elif context.link:
+                fg = 159  # Teal
 
         elif context.in_statusbar:
-            fg, bg, attr = self.verify_statusbar(context, fg, bg, attr)
+            if context.permissions:
+                if context.good:
+                    fg = 159  # Teal
+                elif context.bad:
+                    fg = 203  # Red
+            if context.marked:
+                attr |= bold | reverse
+                fg = 222  # Yellow
+            if context.message:
+                if context.bad:
+                    attr |= bold
+                    fg = 203  # Red
+            if context.loaded:
+                bg = self.progress_bar_color
+            if context.vcsinfo:
+                fg = 116  # Blue
+                attr &= ~bold
+            if context.vcscommit:
+                fg = 222  # Yellow
+                attr &= ~bold
 
-        # Highlight text matches
-        if context.text and context.highlight:
-            attr |= reverse
+        if context.text:
+            if context.highlight:
+                attr |= reverse
 
-        # Task view
         if context.in_taskview:
-            fg, bg, attr = self.verify_taskview(context, fg, bg, attr)
+            if context.title:
+                fg = 116  # Blue
 
-        # VCS file or remote
+            if context.selected:
+                attr |= reverse
+
+            if context.loaded:
+                if context.selected:
+                    fg = self.progress_bar_color
+                else:
+                    bg = self.progress_bar_color
+
         if context.vcsfile and not context.selected:
-            fg, bg, attr = self.verify_vcsfile(context, fg, bg, attr)
+            attr &= ~bold
+            if context.vcsconflicts:
+                fg = 219  # Pink
+            elif context.vcschanged:
+                fg = 203  # Red
+            elif context.vcsunknown:
+                fg = 203  # Red
+            elif context.vcsstaged:
+                fg = 151  # Green
+            elif context.vcssync:
+                fg = 151  # Green
+            elif context.vcsignored:
+                fg = 186  # Surface2
+
         elif context.vcsremote and not context.selected:
-            fg, bg, attr = self.verify_vcsremote(context, fg, bg, attr)
+            attr &= ~bold
+            if context.vcssync:
+                fg = 151  # Green
+            elif context.vcsbehind:
+                fg = 203  # Red
+            elif context.vcsahead:
+                fg = 116  # Blue
+            elif context.vcsdiverged:
+                fg = 219  # Pink
+            elif context.vcsunknown:
+                fg = 203  # Red
 
         return fg, bg, attr
---
+++ ranger/plugins/devicons.py
@@ -0,0 +1,308 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Devicons plugin for ranger
+# Adds file type icons to ranger file browser
+
+import ranger.api
+from ranger.core.linemode import LinemodeBase
+
+# File extension to icon mapping
+EXTENSION_ICONS = {
+    # Programming languages
+    'py': '',
+    'js': '',
+    'ts': '',
+    'jsx': '',
+    'tsx': '',
+    'html': '',
+    'css': '',
+    'scss': '',
+    'sass': '',
+    'less': '',
+    'php': '',
+    'rb': '',
+    'go': '',
+    'rs': '',
+    'c': '',
+    'cpp': '',
+    'cc': '',
+    'cxx': '',
+    'h': '',
+    'hpp': '',
+    'java': '',
+    'kt': '',
+    'swift': '',
+    'cs': '',
+    'vb': '',
+    'fs': '',
+    'clj': '',
+    'cljs': '',
+    'scala': '',
+    'hs': '',
+    'elm': '',
+    'lua': '',
+    'pl': '',
+    'r': '',
+    'jl': '',
+    'dart': '',
+    'vim': '',
+    'sh': '',
+    'bash': '',
+    'zsh': '',
+    'fish': '',
+    'ps1': '',
+    'bat': '',
+    'cmd': '',
+    
+    # Markup and data
+    'xml': '',
+    'json': '',
+    'yaml': '',
+    'yml': '',
+    'toml': '',
+    'ini': '',
+    'cfg': '',
+    'conf': '',
+    'md': '',
+    'rst': '',
+    'tex': '',
+    'txt': '',
+    
+    # Archives
+    'zip': '',
+    'rar': '',
+    '7z': '',
+    'tar': '',
+    'gz': '',
+    'bz2': '',
+    'xz': '',
+    'deb': '',
+    'rpm': '',
+    'pkg': '',
+    'dmg': '',
+    'iso': '',
+    
+    # Images
+    'png': '',
+    'jpg': '',
+    'jpeg': '',
+    'gif': '',
+    'bmp': '',
+    'svg': '',
+    'ico': '',
+    'tiff': '',
+    'webp': '',
+    'raw': '',
+    
+    # Audio
+    'mp3': '',
+    'wav': '',
+    'flac': '',
+    'aac': '',
+    'ogg': '',
+    'wma': '',
+    'm4a': '',
+    
+    # Video
+    'mp4': '',
+    'avi': '',
+    'mkv': '',
+    'mov': '',
+    'wmv': '',
+    'flv': '',
+    'webm': '',
+    'm4v': '',
+    
+    # Documents
+    'pdf': '',
+    'doc': '',
+    'docx': '',
+    'xls': '',
+    'xlsx': '',
+    'ppt': '',
+    'pptx': '',
+    'odt': '',
+    'ods': '',
+    'odp': '',
+    
+    # Fonts
+    'ttf': '',
+    'otf': '',
+    'woff': '',
+    'woff2': '',
+    'eot': '',
+    
+    # System
+    'log': '',
+    'tmp': '',
+    'cache': '',
+    'lock': '',
+    'pid': '',
+    'sock': '',
+    
+    # Git
+    'gitignore': '',
+    'gitmodules': '',
+    'gitattributes': '',
+}
+
+# Filename to icon mapping
+FILENAME_ICONS = {
+    # Config files
+    'Dockerfile': '',
+    'docker-compose.yml': '',
+    'docker-compose.yaml': '',
+    'Makefile': '',
+    'CMakeLists.txt': '',
+    'package.json': '',
+    'package-lock.json': '',
+    'yarn.lock': '',
+    'Cargo.toml': '',
+    'Cargo.lock': '',
+    'go.mod': '',
+    'go.sum': '',
+    'requirements.txt': '',
+    'Pipfile': '',
+    'Pipfile.lock': '',
+    'setup.py': '',
+    'setup.cfg': '',
+    'pyproject.toml': '',
+    'tox.ini': '',
+    'Gemfile': '',
+    'Gemfile.lock': '',
+    'composer.json': '',
+    'composer.lock': '',
+    'webpack.config.js': '',
+    'rollup.config.js': '',
+    'vite.config.js': '',
+    'tsconfig.json': '',
+    'jsconfig.json': '',
+    '.eslintrc': '',
+    '.eslintrc.js': '',
+    '.eslintrc.json': '',
+    '.prettierrc': '',
+    '.babelrc': '',
+    'babel.config.js': '',
+    '.editorconfig': '',
+    '.gitignore': '',
+    '.gitmodules': '',
+    '.gitattributes': '',
+    'LICENSE': '',
+    'README.md': '',
+    'README.rst': '',
+    'README.txt': '',
+    'CHANGELOG.md': '',
+    'CONTRIBUTING.md': '',
+    '.env': '',
+    '.env.example': '',
+    '.env.local': '',
+    
+    # IDE files
+    '.vscode': '',
+    '.idea': '',
+    '.vs': '',
+    
+    # OS files
+    '.DS_Store': '',
+    'Thumbs.db': '',
+    'desktop.ini': '',
+}
+
+# Directory icons
+DIRECTORY_ICONS = {
+    '.git': '',
+    '.github': '',
+    '.vscode': '',
+    '.idea': '',
+    'node_modules': '',
+    '__pycache__': '',
+    '.pytest_cache': '',
+    'venv': '',
+    'env': '',
+    '.env': '',
+    'dist': '',
+    'build': '',
+    'target': '',
+    'bin': '',
+    'lib': '',
+    'include': '',
+    'src': '',
+    'test': '',
+    'tests': '',
+    'docs': '',
+    'doc': '',
+    'assets': '',
+    'static': '',
+    'public': '',
+    'images': '',
+    'img': '',
+    'icons': '',
+    'fonts': '',
+    'css': '',
+    'js': '',
+    'scss': '',
+    'sass': '',
+    'less': '',
+    'config': '',
+    'configs': '',
+    'scripts': '',
+    'tools': '',
+    'utils': '',
+    'helpers': '',
+    'components': '',
+    'pages': '',
+    'views': '',
+    'templates': '',
+    'layouts': '',
+    'partials': '',
+    'includes': '',
+    'modules': '',
+    'plugins': '',
+    'extensions': '',
+    'addons': '',
+    'themes': '',
+    'styles': '',
+    'stylesheets': '',
+    'media': '',
+    'uploads': '',
+    'downloads': '',
+    'tmp': '',
+    'temp': '',
+    'cache': '',
+    'logs': '',
+    'log': '',
+    'backup': '',
+    'backups': '',
+    'archive': '',
+    'archives': '',
+}
+
+def get_icon(filename, is_directory=False):
+    """Get icon for a file or directory"""
+    if is_directory:
+        return DIRECTORY_ICONS.get(filename.lower(), '')
+    
+    # Check filename first
+    if filename in FILENAME_ICONS:
+        return FILENAME_ICONS[filename]
+    
+    # Check by extension
+    if '.' in filename:
+        ext = filename.split('.')[-1].lower()
+        return EXTENSION_ICONS.get(ext, '')
+    
+    return ''
+
+class DeviconsLinemode(LinemodeBase):
+    name = "devicons"
+    uses_metadata = False
+
+    def filetitle(self, file, metadata):
+        icon = get_icon(file.basename, file.is_directory)
+        if icon:
+            return f"{icon} {file.basename}"
+        return file.basename
+
+# Register the linemode
+ranger.api.register_linemode(DeviconsLinemode)
