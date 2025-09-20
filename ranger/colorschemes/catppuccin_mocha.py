5d84c2150ecce55b1a35f6bc164a1b1901dad9cb
jos-cab <joseacabrera@protonmail.com>
2025-09-20 13:20:05 -0300

    Fix ranger devicons linemode and update Catppuccin Mocha colors
    - Fix devicons plugin registration using proper @ranger.api.register_linemode decorator
    - Use WideString for proper icon display in ranger
    - Update Catppuccin Mocha colorscheme with proper color codes from kitty config
    - Map colors to terminal color indices (0-15) instead of arbitrary numbers
    - Remove invalid default_linemode setting from plugins.conf
    - Add keybindings Mi/Mf to toggle between devicons and filename linemodes
    - Colors now match the actual Catppuccin Mocha palette used in kitty

--- ranger/colorschemes/catppuccin_mocha.py
+++ ranger/colorschemes/catppuccin_mocha.py
@@ -5,7 +5,7 @@ from ranger.gui.colorscheme import ColorScheme
 from ranger.gui.color import *
 
 class CatppuccinMocha(ColorScheme):
-    progress_bar_color = 117  # Lavender
+    progress_bar_color = 4  # Blue (#89b4fa)
 
     def use(self, context):
         fg, bg, attr = default_colors
@@ -19,88 +19,88 @@ class CatppuccinMocha(ColorScheme):
             else:
                 attr = normal
             if context.empty or context.error:
-                fg = 203  # Red
+                fg = 1  # Red (#f38ba8)
             if context.border:
-                fg = 186  # Surface2
+                fg = 8  # Surface2 (#585b70)
             if context.media:
                 if context.image:
-                    fg = 222  # Yellow
+                    fg = 3  # Yellow (#f9e2af)
                 else:
-                    fg = 219  # Pink
+                    fg = 5  # Pink (#f5c2e7)
             if context.container:
-                fg = 203  # Red
+                fg = 1  # Red (#f38ba8)
             if context.directory:
                 attr |= bold
-                fg = 116  # Blue
+                fg = 4  # Blue (#89b4fa)
             elif context.executable and not \
                     any((context.media, context.container,
                         context.fifo, context.socket)):
                 attr |= bold
-                fg = 151  # Green
+                fg = 2  # Green (#a6e3a1)
             if context.socket:
-                fg = 219  # Pink
+                fg = 5  # Pink (#f5c2e7)
                 attr |= bold
             if context.fifo or context.device:
-                fg = 222  # Yellow
+                fg = 3  # Yellow (#f9e2af)
                 if context.device:
                     attr |= bold
             if context.link:
-                fg = context.good and 159 or 219  # Teal or Pink
+                fg = context.good and 6 or 5  # Teal (#94e2d5) or Pink (#f5c2e7)
             if context.bad:
-                bg = 203  # Red
+                bg = 1  # Red (#f38ba8)
             if context.tag_marker and not context.selected:
                 attr |= bold
-                if fg in (203, 219):  # Red, Pink
-                    fg = 205  # Text
+                if fg in (1, 5):  # Red, Pink
+                    fg = 15  # Text (#a6adc8)
                 else:
-                    fg = 203  # Red
+                    fg = 1  # Red (#f38ba8)
             if not context.selected and (context.cut or context.copied):
-                fg = 237  # Base
+                fg = 0  # Surface0 (#45475a)
                 attr |= bold
             if context.main_column:
                 if context.selected:
                     attr |= bold
                 if context.marked:
                     attr |= bold
-                    fg = 222  # Yellow
+                    fg = 3  # Yellow (#f9e2af)
             if context.badinfo:
                 if attr & reverse:
-                    bg = 203  # Red
+                    bg = 1  # Red (#f38ba8)
                 else:
-                    fg = 203  # Red
+                    fg = 1  # Red (#f38ba8)
 
         elif context.in_titlebar:
             attr |= bold
             if context.hostname:
-                fg = context.bad and 203 or 151  # Red or Green
+                fg = context.bad and 1 or 2  # Red (#f38ba8) or Green (#a6e3a1)
             elif context.directory:
-                fg = 116  # Blue
+                fg = 4  # Blue (#89b4fa)
             elif context.tab:
                 if context.good:
-                    bg = 151  # Green
+                    bg = 2  # Green (#a6e3a1)
             elif context.link:
-                fg = 159  # Teal
+                fg = 6  # Teal (#94e2d5)
 
         elif context.in_statusbar:
             if context.permissions:
                 if context.good:
-                    fg = 159  # Teal
+                    fg = 6  # Teal (#94e2d5)
                 elif context.bad:
-                    fg = 203  # Red
+                    fg = 1  # Red (#f38ba8)
             if context.marked:
                 attr |= bold | reverse
-                fg = 222  # Yellow
+                fg = 3  # Yellow (#f9e2af)
             if context.message:
                 if context.bad:
                     attr |= bold
-                    fg = 203  # Red
+                    fg = 1  # Red (#f38ba8)
             if context.loaded:
                 bg = self.progress_bar_color
             if context.vcsinfo:
-                fg = 116  # Blue
+                fg = 4  # Blue (#89b4fa)
                 attr &= ~bold
             if context.vcscommit:
-                fg = 222  # Yellow
+                fg = 3  # Yellow (#f9e2af)
                 attr &= ~bold
 
         if context.text:
@@ -109,7 +109,7 @@ class CatppuccinMocha(ColorScheme):
 
         if context.in_taskview:
             if context.title:
-                fg = 116  # Blue
+                fg = 4  # Blue (#89b4fa)
 
             if context.selected:
                 attr |= reverse
@@ -123,29 +123,29 @@ class CatppuccinMocha(ColorScheme):
         if context.vcsfile and not context.selected:
             attr &= ~bold
             if context.vcsconflicts:
-                fg = 219  # Pink
+                fg = 5  # Pink (#f5c2e7)
             elif context.vcschanged:
-                fg = 203  # Red
+                fg = 1  # Red (#f38ba8)
             elif context.vcsunknown:
-                fg = 203  # Red
+                fg = 1  # Red (#f38ba8)
             elif context.vcsstaged:
-                fg = 151  # Green
+                fg = 2  # Green (#a6e3a1)
             elif context.vcssync:
-                fg = 151  # Green
+                fg = 2  # Green (#a6e3a1)
             elif context.vcsignored:
-                fg = 186  # Surface2
+                fg = 8  # Surface2 (#585b70)
 
         elif context.vcsremote and not context.selected:
             attr &= ~bold
             if context.vcssync:
-                fg = 151  # Green
+                fg = 2  # Green (#a6e3a1)
             elif context.vcsbehind:
-                fg = 203  # Red
+                fg = 1  # Red (#f38ba8)
             elif context.vcsahead:
-                fg = 116  # Blue
+                fg = 4  # Blue (#89b4fa)
             elif context.vcsdiverged:
-                fg = 219  # Pink
+                fg = 5  # Pink (#f5c2e7)
             elif context.vcsunknown:
-                fg = 203  # Red
+                fg = 1  # Red (#f38ba8)
 
         return fg, bg, attr
